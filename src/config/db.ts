3e7824bb8a0fcf27954a75872986487d842e50a8
Asitha Lakshan <asitha.pwc@gmail.com>
2023-03-01 16:27:15 +0530

    Set db config

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-west-1'
+});
+
+const db = new AWS.DynamoDB.DocumentClient();
+
+export default db;
